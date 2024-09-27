a87ae3a979414bb7296950d2e6938ee9ddf2bfbb
Tauno Erik <sedumacre@gmail.com>
2024-09-27 16:17:52 +0300

    touch

--- player.gd
+++ player.gd
@@ -9,17 +9,34 @@ var screen_size # Size of the game window.
 
 var player_size
 
+var velocity = Vector2.ZERO
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 	player_size = $CollisionShape2D.shape.get_rect().size
 	hide()  # player will be hidden when the game starts
+	
+
+func _input(event):
+	if event is InputEventScreenTouch and event.pressed == true:
+		var touch_position = event.position  # the position of the touch
+		print(touch_position)
+		#print(position) # player
+		if touch_position.x < position.x:
+			velocity.x -= 1  # move_left
+		else:
+			velocity.x += 1 # move_right
+		if touch_position.y < position.y:
+			velocity.y -= 1  # move_up
+		else:
+			velocity.y += 1  # move_down
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var velocity = Vector2.ZERO # The player's movement vector. (0, 0)
-	
+	#var velocity = Vector2.ZERO # The player's movement vector. (0, 0)
+
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -50,6 +67,8 @@ func _process(delta: float) -> void:
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "up"
 		$AnimatedSprite2D.flip_v = velocity.y > 0  # true or false
+		
+	velocity = Vector2.ZERO
 
 
 # emit hit signal
